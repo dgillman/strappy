4971c8df15176b74a4b26c584c7de63bcfdb46e2
Duffy Gillman <duffy@rsmart.com>
2013-01-17 17:06:39 -0700

    updated README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+Strappy creates a MIME multipart message for use with [CloutInit](https://help.ubuntu.com/community/CloudInit). Files are read from a template directory. If the file ends in .erb it is processed as an embedded Ruby template using the Binding context passed in by the caller of the processTemplates method. Files are assigned a MIME type based on comparing their first lines to those expected of the various file types. Any file that does not match one of the mappings will be given the text/plain type.
+
+* Usage
+
+1. Create a new Strappy object. You must pass it a directory path.
+2. Initialize any variables that you would like to make available to the ERB processor when processing templates
+3. Call strappy.processTemplates with a [Binding](http://www.ruby-doc.org/core-1.9.3/Binding.html) object. Use a call to binding() to get the current context.
+
+```
+  templateDirectory = "/path/to/a/directory"
+  strappy = Strappy.new(templateDirectory)
+  contextVariable = "this is a test"
+  result = strappy.processTemplates(binding())		
+```
--- lib/strappy.rb
+++ lib/strappy.rb
@@ -2,6 +2,11 @@ require 'mime'
 require 'erb'
 require 'tmpdir'
 
+# Strappy creates a MIME multipart message for use with CloutInit
+# Files are read from a template directory. If the file ends in .erb it is processed as an embedded Ruby template
+# using the Binding context passed in by the caller of the processTemplates method. Files are assigned a MIME type
+# based on comparing their first lines to those expected of the various file types. Any file that does not match
+# one of the mappings will be given the text/plain type
 class Strappy
 
   @@starts_with_mappings = {
@@ -13,10 +18,12 @@ class Strappy
     '#cloud-boothook' => 'text/cloud-boothook'
   }
 
+  # requires a directory containing templates and files to be place in the message
   def initialize( templateDir )
     @templateDir = templateDir
   end
 
+  # resolves the type of file to one of those anticipated by CloutInit
   def getType( part )
     @@starts_with_mappings.each do |k, v|
       if part.start_with?(k) then
@@ -27,6 +34,7 @@ class Strappy
     return nil
   end
 
+  # reads a .erb file and returns the template object
   def readTemplate (fileName)
     f = File.new(fileName)
     template = ERB.new(f.read)
@@ -35,11 +43,18 @@ class Strappy
     return template
   end
 
+  # processes the template directory, returning a MIME:MultipartMedia::Mixed message object
   def processTemplates(binding = Kernel.binding)
+
+  	# .erb templates are processed into a temp directory; non-.erb files will be copied over
     Dir.mktmpdir ("strappy") { |dir|  
       Dir.foreach( @templateDir ) { |entry|
         templateFile = "#{@templateDir}/#{entry}"
+
+        # skip anything that is not a file
         unless File.directory? (templateFile) then
+
+        	# process templates
           if (matchData = entry.match('(.*)\.erb')) then  
             outName = matchData.captures[0]
 
@@ -48,23 +63,31 @@ class Strappy
               tempFile.write(template.result(binding))
             end
           else
+          	# copy anything which is not a template directly
             FileUtils.cp templateFile, "#{dir}/#{entry}"
           end
         end
       }
 
+      # new message object
       message = MIME::MultipartMedia::Mixed.new
 
+      # process each file in the temp directory
       Dir.foreach ( dir ) { |entry|
 
         fileName = "#{dir}/#{entry}"
 
         unless File.directory? (fileName) then
 
+        	# read file content
           f = File.new ("#{dir}/#{entry}")
           content = f.read
           f.close
+
+          # determine type, defaulting to 'text/plain'
           type = "text/plain" unless (type = getType(content))
+
+          # create a new part for the message and add it
           part = MIME::TextMedia.new(content, type)
 
           message.add_entity(part)
